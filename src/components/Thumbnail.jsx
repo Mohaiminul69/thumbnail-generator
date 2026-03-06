5aace0978a83c2c1d0c385d75ba7c74453e585bd
mohaiminul69 <mohaiminul69@gmail.com>
2026-03-06 13:58:58 +0600

    icons added.

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import CppIcon from "./icons/cpp-icon";
+import JavaScriptIcon from "./icons/javascript-icon";
+import RubyIcon from "./icons/ruby-icon";
+import PythonIcon from "./icons/python-icon";
 
 const Thumbnail = () => {
   const [texts, setTexts] = useState({
@@ -21,29 +25,52 @@ const Thumbnail = () => {
   return (
     <div className="bg-[#161616] relative aspect-video w-225 m-auto overflow-hidden shadow-[inset_0_0_0_12px_#9C2426]">
       <div className="bg-[radial-gradient(circle_at_70%_50%,rgba(156,36,38,0.4)_20%,rgba(22,22,22,1)_85%)] border-8 border-[#9C2426] rounded-3xl p-8 flex justify-between items-center h-full">
-        <div className="content space-y-3 text-white overflow-visible z-10 shrink w-1/2">
-          <h1
-            onClick={() => setEditTextType("title")}
-            className="text-4xl whitespace-nowrap cursor-pointer"
-          >
-            {texts.title}
-          </h1>
-          <p
-            onClick={() => setEditTextType("subtitle")}
-            className="text-3xl cursor-pointer"
-          >
-            {texts.subtitle}
-          </p>
-          {editTextType && (
-            <input
-              type="text"
-              autoFocus
-              onChange={updateText}
-              onKeyDown={handleKeyDown}
-              defaultValue={texts[editTextType]}
-              className="mt-3 w-full bg-[#0f0f0f] family-[sans-serif] text-white text-lg px-4 py-2 rounded-lg border border-[#9C2426] outline-none focus:ring-2 focus:ring-[#9C2426] placeholder-gray-400"
-            />
-          )}
+        <div className="h-[80%] flex flex-col justify-between space-y-3 text-white overflow-visible z-10 shrink w-1/2">
+          <div className="space-y-4">
+            <h1
+              onClick={() => setEditTextType("title")}
+              className="text-4xl whitespace-nowrap cursor-pointer"
+            >
+              {texts.title}
+            </h1>
+            <p
+              onClick={() => setEditTextType("subtitle")}
+              className="text-3xl cursor-pointer"
+            >
+              {texts.subtitle}
+            </p>
+            {editTextType && (
+              <input
+                type="text"
+                autoFocus
+                onChange={updateText}
+                onKeyDown={handleKeyDown}
+                defaultValue={texts[editTextType]}
+                className="mt-3 w-full bg-[#0f0f0f] family-[sans-serif] text-white text-lg px-4 py-2 rounded-lg border border-[#9C2426] outline-none focus:ring-2 focus:ring-[#9C2426] placeholder-gray-400"
+              />
+            )}
+          </div>
+          <div className="flex gap-8 text-white">
+            <div className="flex flex-col items-center">
+              <CppIcon className="size-16" />
+              <p className="mt-2 text-lg">C++</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <JavaScriptIcon className="size-16" />
+              <p className="mt-2 text-lg">Java Script</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <RubyIcon className="size-16" />
+              <p className="mt-2 text-lg">Ruby</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <PythonIcon className="size-16" />
+              <p className="mt-2 text-lg">Python</p>
+            </div>
+          </div>
         </div>
         <div className="bg-[#9C2426] size-100 rounded-full shrink-0 shadow-[inset_0_0_0_10px_#9C2426,0_0_40px_rgba(185,28,28,0.6)]" />
         <div className="bg-[#9C2426] size-30 rounded-full absolute left-87.5 -bottom-17.5 shadow-[inset_0_0_0_10px_#9C2426,0_0_40px_rgba(185,28,28,0.6)]" />
