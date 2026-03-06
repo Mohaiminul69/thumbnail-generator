9a1d464c619b2789dad52da3ed3bf4b64b43a48d
mohaiminul69 <mohaiminul69@gmail.com>
2026-03-06 12:36:49 +0600

    made the thumbnail background.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Thumbnail from "./components/thumbnail";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-[#0b090a] p-16">
+      <Thumbnail />
+    </div>
+  );
+};
+
+export default App;
